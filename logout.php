3e86cce074f40a0615ee5addcf3fc37544b4c643
Ivan-Wabuyobo <wabuyoboivan@gmail.com>
2023-04-03 20:04:42 +0300

    added logout

---
+++ logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+$user =  $_SESSION['user']['id'];
+$transaction_id = "#".date('Ym').time();
+include "dbconnect.php";
+$sql = "INSERT INTO `log`(`transaction_id`, `transaction`, `user`) VALUES ('$transaction_id', 'Logged out successfully', '$user')";
+$conn->query($sql);
+
+// remove all session variables
+session_unset();
+
+// redirect the user to the index page
+header("location:login.php");
+
+?>
